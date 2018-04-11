07180eb872cf537beae626b93eba355a770c82e6
Robert Cleric <robert.cleric@gmail.com>
2018-04-11 18:37:07 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+
+import { AlertModule } from 'ngx-bootstrap';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AlertModule.forRoot(),
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
