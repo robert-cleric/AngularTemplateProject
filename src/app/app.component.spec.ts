a42ebc3c61df950f8226a4bbac956d33a2054cef
Robert Cleric <robert.cleric@gmail.com>
2018-05-16 19:45:45 -0400

    Adding unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,15 +17,5 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'atp'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('atp');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to atp!');
-  }));
+
 });
--- src/app/details/details.component.spec.ts
+++ src/app/details/details.component.spec.ts
@@ -1,22 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailsComponent } from './details.component';
+import { DataService } from '../core/data.service';
+
+class MockDataService {
+  getDogs = {};
+}
 
 describe('DetailsComponent', () => {
   let component: DetailsComponent;
   let fixture: ComponentFixture<DetailsComponent>;
 
+  // let dataService;
+  // beforeEach(async(() => {
+  //   TestBed.configureTestingModule({
+  //     providers: [DetailsComponent, {provide: DataService, useClass: MockDataService}]
+  //   });
+
+  //   component = TestBed.get(DetailsComponent);
+  //   dataService = TestBed.get(DataService);
+  // }));
+
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+
+  let dataServiceStub: Partial<DataService>;
+  let dataService: DataService;
+
   beforeEach(async(() => {
+    dataServiceStub = {
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ DetailsComponent ]
+      declarations: [DetailsComponent],
+       providers: [{ provide: DataService, useValue: dataServiceStub }]
+      // providers: [{provide: DataService, useValue: MockDataService}]
     })
-    .compileComponents();
+      .compileComponents();
+
+      fixture = TestBed.createComponent(DetailsComponent);
+      component = fixture.componentInstance;
+       dataService = fixture.debugElement.injector.get(DataService);
+      // dataService = TestBed.get(DataService);
+      // fixture.detectChanges();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+
   });
 
   it('should create', () => {
